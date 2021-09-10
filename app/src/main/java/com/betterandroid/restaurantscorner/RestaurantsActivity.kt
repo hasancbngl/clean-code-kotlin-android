b29e482824dc8d18191edad465ebcbe7cec5114b
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-09-10 22:25:09 +0300

    refactor using map and filter operators

--- app/src/main/java/com/betterandroid/restaurantscorner/RestaurantsActivity.kt
+++ app/src/main/java/com/betterandroid/restaurantscorner/RestaurantsActivity.kt
@@ -111,33 +111,21 @@ class RestaurantsActivity : AppCompatActivity() {
         return filteredRestaurants
     }
 
-    private fun parseRestaurants(response: RestaurantListResponse): ArrayList<Restaurant> {
-        val restaurants = response.restaurants
-        val parsedRestaurants = arrayListOf<Restaurant>()
-
-        if (restaurants != null) {
-            for (responseRestaurant in restaurants) {
-                if (responseRestaurant.name != null
-                    && responseRestaurant.imageUrl != null
-                ) {
-                    val location = SimpleLocation(
-                        responseRestaurant.locationLatitude,
-                        responseRestaurant.locationLongitude
-                    )
-                    parsedRestaurants.add(
-                        Restaurant(
-                            id = responseRestaurant.id,
-                            name = responseRestaurant.name,
-                            imageUrl = responseRestaurant.imageUrl,
-                            location = location,
-                            closingHour = responseRestaurant.closingHour,
-                            type = responseRestaurant.type
-                        )
-                    )
-                }
-            }
-        }
-        return parsedRestaurants
+    private fun parseRestaurants(response: RestaurantListResponse): List<Restaurant> {
+        return response.restaurants
+            ?.filter {
+                it.name != null && it.imageUrl != null
+            }?.map {
+                return@map Restaurant(
+                    id = it.id,
+                    name = it.name!!,
+                    imageUrl = it.imageUrl!!,
+                    location = SimpleLocation(it.locationLatitude, it.locationLongitude),
+                    closingHour = it.closingHour,
+                    type = it.type
+                )
+            }.orEmpty()
+        //if couldnt filter it and its null return empty
     }
 
     override fun onDestroy() {
